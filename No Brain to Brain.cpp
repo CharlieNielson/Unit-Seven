bdc71425cbffadf3454a657554be0ab4a43478db
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-28 14:40:19 -0600

    Update No Brain to Brain.cpp

--- No Brain to Brain.cpp
+++ No Brain to Brain.cpp
@@ -1,3 +1,20 @@
-#include <iostream>;
-useing namespace std;
+#include <iostream>
+using namespace std;
 
+//int has less storage than double.
+// cout << &variable; gives us the variable address.
+//pointers have a value of a memory address, and are defined by a *.
+
+//const int * ptr; forbids changing of the int.
+//int * const ptr; //constant memory location.
+//const int * const ptr;//constant both.
+
+//arrays are automaticaly a pointer.
+//arrays are constant, but pointers are not.
+
+int main() {
+
+    //_
+
+    return 0;
+}
