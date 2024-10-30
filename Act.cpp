2ec306fa06f466ae9e7f95cc90b71c7d66c11ec0
CharlieNielson <charlienielson@mail.weber.edu>
2024-10-30 09:01:46 -0600

    Update Act.cpp

--- Act.cpp
+++ Act.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 
 int main() {
-
-    int num = 7;
+    /*int num = 7;
     int *ptr = &num;
 
     double speed = 55.7;
@@ -18,7 +17,28 @@ int main() {
     *ptr = 10;
     cout << "Thing: " << num << endl;
 
-    cout << "Speed: " << *dPtr << endl;
+    cout << "Speed: " << *dPtr << endl;*/
+
+    /*int *values;
+    int size;
+    cout << "Enter the number of values greater than 3: ";
+    cin >> size;
+    values = new int[size];
+    for(int i = 0; i < size; i++){
+        values [i] = i;
+    }
+
+    cout << values << endl;
+    cout << *values << endl; 	
+    cout << values[2] << endl;
+   	cout << *(values+2) << endl;
+    delete [] values;*/
+
+    auto values;
+    values = new int[5];
+
+    cout << *ptr;
+    
 
     return 0;
 }
